ad1584bae018284d26e3f5dd6c771f26d923c98d
Theon <theon6@outlook.com>
2026-05-17 11:47:05 +0800

    add .gitignore BeanDefination PropertyValues

---
+++ src/BeanDefination.java
@@ -0,0 +1,48 @@
+public class BeanDefination {
+
+    private String BeanName;
+    private Class<?> beanClass;
+    private String initMethodName;
+    private String destroyMethodName;
+    private PropertyValues propertyValues;
+
+    public String getBeanName() {
+        return BeanName;
+    }
+
+    public void setBeanName(String beanName) {
+        BeanName = beanName;
+    }
+
+    public Class<?> getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class<?> beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public String getDestroyMethodName() {
+        return destroyMethodName;
+    }
+
+    public void setDestroyMethodName(String destroyMethodName) {
+        this.destroyMethodName = destroyMethodName;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
+    }
+}
