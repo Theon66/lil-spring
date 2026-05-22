2a9f856403d3373e7b1b2d3fa6ccc99a7689a3c7
Theon <theon6@outlook.com>
2026-05-22 11:19:34 +0800

    创建Bean、依赖注入

--- src/com/theon/BeanFactory.java
+++ src/com/theon/BeanFactory.java
@@ -4,31 +4,43 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Enumeration;
-import java.util.Properties;
+import java.util.*;
 
 public class BeanFactory {
 
-    public static void main() {
+    public static void main(String[] args) {
+        Map<String,BeanDefination> beanMap=new HashMap<>();
         try {
             // 从配置文件中读取Bean和依赖关系
             Properties properties = loadProperties();
-            Enumeration<?> names = properties.propertyNames();
-            while (names.hasMoreElements()) {
-                String name = (String)names.nextElement();
+            Enumeration<?> propNames = properties.propertyNames();
+            while (propNames.hasMoreElements()) {
+                String name = (String)propNames.nextElement();
                 String value = properties.getProperty(name);
-                System.out.println(name + ": " + value);
+//                System.out.println(name + ": " + value);
+                // 判断是创建Bean还是注入依赖
+                if (name.contains("dependency")) {
+                    String[] arr = name.split("\\.");
+                    String beanName=arr[0];
+                    String dependName = value;
+                    Dependency dependency = new Dependency();
+                    dependency.setName(dependName);
+                    dependency.setValue(beanMap.get(dependName));
+                    if (beanMap.get(beanName).getDependencies() == null) {
+                        List<Dependency> dependencyList = new ArrayList<>();
+                        dependencyList.add(dependency);
+                        beanMap.get(beanName).setDependencies(dependencyList);
+                    } else {
+                        beanMap.get(beanName).getDependencies().add(dependency);
+                    }
+                } else {
+                    BeanDefination bean =  new BeanDefination();
+                    bean.setBeanName(name);
+                    bean.setBeanClass(Class.forName(value));
+                    beanMap.put(name, bean);
+                }
             }
-            // 反射实例化对象
-            Class<?> clazz = Class.forName("com.theon.User");
-            BeanDefination bean = new BeanDefination();
-            Object person = clazz.getConstructor().newInstance();
-            // 依赖注入
-            // 读取配置
-
-        } catch (ClassNotFoundException | NoSuchMethodException
-                 | InvocationTargetException | InstantiationException |
-                 IllegalAccessException e) {
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -41,7 +53,8 @@ public class BeanFactory {
     static Properties loadProperties() throws IOException {
         // 从文件加载配置
         Properties properties = new Properties();
-        FileInputStream fileInputStream = new FileInputStream("beans.properties");
+        FileInputStream fileInputStream =
+                new FileInputStream("src/com/theon/beans.properties");
         properties.load(fileInputStream);
         fileInputStream.close();
         return properties;
